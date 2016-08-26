52260f99fcee35a673995f7f188ad903dbebac76
perkyguy420 <garrett.a.scott@gmail.com>
2016-08-26 08:54:45 -0700

    Node acts as a TCP server instead of a client, will send things to all of the Xpress/Other clients responds with the first answer. Can add-on to show a listing of all available clients and communicate to them, but that's for a future update.

--- express-xpress-node.js
+++ express-xpress-node.js
@@ -3,31 +3,88 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-var client = {};
-function defaultDataCallback(data){
-    console.log('This is from Xpress:', data.toString());
+var clients = [];
+
+const XPRESS_DATA_STRING = "It's not about money, it's about sending a message.\r\n";
+
+function noClients() {
+    return clients.length == 0;
 }
-client = net.createConnection({host: "192.168.1.3",port: 1337}, () => {
-  //'connect' listener
-  console.log('connected to Xpress!');
-  client.write('\n');
-});
-client.on('data', defaultDataCallback);
-client.on('end', () => {
-  console.log('disconnected from Xpress');
-});
+
+function sendToClients(message, options) {
+    options = options || {};
+    clients.forEach(function(socket) {
+        if (options.callback != null) {
+            if (!options.addToXpressOnly || (options.addToXpressOnly && socket.is_xpress)) {
+                socket.removeAllListeners('data')
+                socket.on('data', function(d) {
+                    options.callback(d, socket);
+                });
+            }
+        } else {
+            socket.removeAllListeners('data')
+
+            socket.on('data', (data) => {
+                defaultDataCallback(data, socket);
+            })
+        }
+        socket.write(message);
+    });
+
+    clients.forEach(function(socket) {
+        if (socket.destroyed) {
+            // console.log(socket);
+            clients.splice(clients.indexOf(socket), 1);
+        }
+    });
+    // Log it to the server output too
+    process.stdout.write(message)
+}
+
+function defaultDataCallback(data, socket) {
+    if (data.toString() == XPRESS_DATA_STRING) {
+        socket.is_xpress = true;
+        socket.name = "Xpress-" + socket.remoteAddress.match(/\d+$/)[0];
+    }
+    console.log("<" + socket.name + ">", data.toString());
+}
+
+
+var server = net.createServer((socket) => {
+    socket.name = socket.remoteAddress + ":" + socket.remotePort
+    console.log('[' + socket.name + ']');
+    clients.push(socket);
+    socket
+        .on('data', (data) => {
+            defaultDataCallback(data, socket);
+        })
+        .on('end', () => {
+            console.log('{' + socket.name + "}");
+        })
+        .on('error', (e) => {
+            console.log('Caught some error, disconnected?', e);
+            socket.destroy();
+            socket.end();
+        })
+}).listen(1337);
 
 app
-    .get('/', function(req, res){
-       res.sendFile(path.join(__dirname+'/express-xpress-node.html'));
+    .get('/', function(req, res) {
+        res.sendFile(path.join(__dirname + '/express-xpress-node.html'));
     })
-    .get('/relay/:relayCommand', function (req, res) {
-        var reply = {msg: ""};
-        if(req.params.relayCommand == "enable"){
-            client.write('relay enable\r\n');
-            reply.msg = "Shields online..."; 
-        } else if(req.params.relayCommand == "disable"){
-            client.write('relay disable\r\n');
+    .get('/relay/:relayCommand', function(req, res) {
+        if (noClients()) {
+            res.send({ msg: 'No Clients...' });
+            return;
+        }
+
+        var reply = { msg: "" };
+        if (req.params.relayCommand == "enable") {
+
+            sendToClients('relay enable\r\n');
+            reply.msg = "Shields online...";
+        } else if (req.params.relayCommand == "disable") {
+            sendToClients('relay disable\r\n');
             reply.msg = "Tractor beam disabled...";
         } else {
             reply.msg = "What?!?!";
@@ -35,29 +92,27 @@ app
 
         res.send(reply);
     })
-    .get('/adcc', function(req, res){
+    .get('/adcc', function(req, res) {
         var adcc_callback;
-        adcc_callback = (data) => {
-            matches = data.toString('utf-8').match('ADC=(\\d+)');
-            if(matches){
-                res.send({msg: "Don't move that dial...or do...or whatever...", value: +matches[1]})
-            } else {
-                res.send({msg: 'No dice!', value: undefined});
+        adcc_callback = (data, client) => {
+            if (res.headersSent) {
+                console.log('Headers sent...');
+                client.removeListener('data', adcc_callback);
+                return;
             }
-            client.removeListener('data', adcc_callback);
+            matches = data.toString('utf-8').match('ADC=(\\d+)');
+            if (matches) {
+                res.send({ msg: "Don't move that dial...or do...or whatever...", value: +matches[1] })
+            } else {
+                res.send({ msg: 'No dice!', value: undefined });
+            }
+
         };
-        client.write('pot value\r\n');
-        client.addListener('data', adcc_callback);
+        sendToClients('pot value\r\n', {
+            callback: adcc_callback,
+            addToXpressOnly: true
+        });
     })
-    .get('/killMe', function (req, res) {
-        res.send({msg: "You have so much to live for!!"});
-    })
-    .get('/killMe/:really', function (req, res) {
-        if(req.params.really == "now"){
-            client.end();
-            throw '(╯°□°)–︻╦╤─ – – – -(*_*)';
-        }
-    })
-    .listen(1337, function(){
+    .listen(8008, function() {
         console.log("Pulling into port");
     });
